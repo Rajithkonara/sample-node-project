8314cade044ff5945c09f9ee993fbecdded55fe8
rajithkonara <rajithkonara@gmail.com>
2020-11-25 17:40:12 +0530

    added soft delete key

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morgan = require('morgan');
 
 const designerRouter = require('./routes/designer');
 
@@ -12,7 +12,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
